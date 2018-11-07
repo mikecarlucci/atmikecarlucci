962204ad047af1d9d60bde2bbf8004184f348e99
Mike Carlucci <mikecarlucci@users.noreply.github.com>
2018-11-06 22:10:20 -0500

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19758,
-	"created_at": "Tue Nov 06 03:10:19 +0000 2018",
+	"tweets": 19761,
+	"created_at": "Wed Nov 07 03:10:19 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,567 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Tue Nov 06 23:08:22 +0000 2018",
+		"id": 1059945617064108000,
+		"id_str": "1059945617064108033",
+		"full_text": "@Hegemommy Unless it's too darn loud😂 https://t.co/oXToUzWf0G",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			37
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Hegemommy",
+					"name": "Jessica Mason Pieklo",
+					"id": 20269254,
+					"id_str": "20269254",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1059945609266896900,
+					"id_str": "1059945609266896897",
+					"indices": [
+						38,
+						61
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DrWu6nGWoAEW-0t.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DrWu6nGWoAEW-0t.jpg",
+					"url": "https://t.co/oXToUzWf0G",
+					"display_url": "pic.twitter.com/oXToUzWf0G",
+					"expanded_url": "https://twitter.com/mikecarlucci/status/1059945617064108033/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 250,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 250,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 250,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1059945609266896900,
+					"id_str": "1059945609266896897",
+					"indices": [
+						38,
+						61
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/DrWu6nGWoAEW-0t.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/DrWu6nGWoAEW-0t.jpg",
+					"url": "https://t.co/oXToUzWf0G",
+					"display_url": "pic.twitter.com/oXToUzWf0G",
+					"expanded_url": "https://twitter.com/mikecarlucci/status/1059945617064108033/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 500,
+							"h": 250,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 500,
+							"h": 250,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 250,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							2,
+							1
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/DrWu6nGWoAEW-0t.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1059932041024700400,
+		"in_reply_to_status_id_str": "1059932041024700416",
+		"in_reply_to_user_id": 20269254,
+		"in_reply_to_user_id_str": "20269254",
+		"in_reply_to_screen_name": "Hegemommy",
+		"user": {
+			"id": 90161583,
+			"id_str": "90161583",
+			"name": "Mike Carlucci",
+			"screen_name": "mikecarlucci",
+			"location": "Boston, MA",
+			"description": "Writing about baseball @OverTheMonster Data Analyst @Northeastern. Co-host @ozminute mike@mikecarlucci.org",
+			"url": "https://t.co/rbUdKXqP9q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/rbUdKXqP9q",
+							"expanded_url": "http://www.sbnation.com/users/mikecarlucci/blog",
+							"display_url": "sbnation.com/users/mikecarl…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4883,
+			"friends_count": 884,
+			"listed_count": 50,
+			"created_at": "Sun Nov 15 13:32:20 +0000 2009",
+			"favourites_count": 46476,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19533,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/90161583/1398534968",
+			"profile_link_color": "0A0AFF",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EDE8ED",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@Hegemommy Unless it's too darn loud😂 https://t.co/oXToUzWf0G"
+	},
+	{
+		"created_at": "Tue Nov 06 11:49:38 +0000 2018",
+		"id": 1059774807183712300,
+		"id_str": "1059774807183712261",
+		"full_text": "Boston Donuts in Ireland #damagedone #teamcarlucci https://t.co/OUKbr9BBNP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			50
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "damagedone",
+					"indices": [
+						25,
+						36
+					]
+				},
+				{
+					"text": "teamcarlucci",
+					"indices": [
+						37,
+						50
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1059774688803483600,
+					"id_str": "1059774688803483649",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/DrUTduwU8AE522g.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrUTduwU8AE522g.jpg",
+					"url": "https://t.co/OUKbr9BBNP",
+					"display_url": "pic.twitter.com/OUKbr9BBNP",
+					"expanded_url": "https://twitter.com/mikecarlucci/status/1059774807183712261/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1059774688803483600,
+					"id_str": "1059774688803483649",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/DrUTduwU8AE522g.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrUTduwU8AE522g.jpg",
+					"url": "https://t.co/OUKbr9BBNP",
+					"display_url": "pic.twitter.com/OUKbr9BBNP",
+					"expanded_url": "https://twitter.com/mikecarlucci/status/1059774807183712261/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 90161583,
+			"id_str": "90161583",
+			"name": "Mike Carlucci",
+			"screen_name": "mikecarlucci",
+			"location": "Boston, MA",
+			"description": "Writing about baseball @OverTheMonster Data Analyst @Northeastern. Co-host @ozminute mike@mikecarlucci.org",
+			"url": "https://t.co/rbUdKXqP9q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/rbUdKXqP9q",
+							"expanded_url": "http://www.sbnation.com/users/mikecarlucci/blog",
+							"display_url": "sbnation.com/users/mikecarl…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4883,
+			"friends_count": 884,
+			"listed_count": 50,
+			"created_at": "Sun Nov 15 13:32:20 +0000 2009",
+			"favourites_count": 46476,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19533,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/90161583/1398534968",
+			"profile_link_color": "0A0AFF",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EDE8ED",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Boston Donuts in Ireland #damagedone #teamcarlucci https://t.co/OUKbr9BBNP"
+	},
+	{
+		"created_at": "Sun Nov 04 21:48:44 +0000 2018",
+		"id": 1059200802307231700,
+		"id_str": "1059200802307231744",
+		"full_text": "CUT MY LIFE INTO PIECES\nTHIS IS A TINY HORSE https://t.co/1XviRITXPu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			44
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1059200795776827400,
+					"id_str": "1059200795776827392",
+					"indices": [
+						45,
+						68
+					],
+					"media_url": "http://pbs.twimg.com/media/DrMJgwoWwAANCYX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrMJgwoWwAANCYX.jpg",
+					"url": "https://t.co/1XviRITXPu",
+					"display_url": "pic.twitter.com/1XviRITXPu",
+					"expanded_url": "https://twitter.com/annetdonahue/status/1059200802307231744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 605,
+							"h": 410,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 605,
+							"h": 410,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 605,
+							"h": 410,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1059200795776827400,
+					"id_str": "1059200795776827392",
+					"indices": [
+						45,
+						68
+					],
+					"media_url": "http://pbs.twimg.com/media/DrMJgwoWwAANCYX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrMJgwoWwAANCYX.jpg",
+					"url": "https://t.co/1XviRITXPu",
+					"display_url": "pic.twitter.com/1XviRITXPu",
+					"expanded_url": "https://twitter.com/annetdonahue/status/1059200802307231744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 605,
+							"h": 410,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 605,
+							"h": 410,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 605,
+							"h": 410,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 22419345,
+			"id_str": "22419345",
+			"name": "Anne T. Donahue",
+			"screen_name": "annetdonahue",
+			"location": "Cambridge/Toronto, Canada",
+			"description": "I wrote a book called Nobody Cares and get to write for a bunch of places I love. I'm also a big fan of the mall. annetdonahue@gmail.com",
+			"url": "https://t.co/osNn3ggUMa",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/osNn3ggUMa",
+							"expanded_url": "http://tinyletter.com/annetdonahue",
+							"display_url": "tinyletter.com/annetdonahue",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 50481,
+			"friends_count": 2090,
+			"listed_count": 793,
+			"created_at": "Sun Mar 01 23:03:16 +0000 2009",
+			"favourites_count": 105375,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 74842,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "7A7171",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1049093300731502596/kESN7z16_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1049093300731502596/kESN7z16_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/22419345/1488402970",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFF7CC",
+			"profile_text_color": "261818",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 611,
+		"favorite_count": 4073,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "CUT MY LIFE INTO PIECES\nTHIS IS A TINY HORSE https://t.co/1XviRITXPu"
+	},
+	{
 		"created_at": "Sun Nov 04 21:48:44 +0000 2018",
 		"id": 1059200802307231700,
 		"id_str": "1059200802307231744",
