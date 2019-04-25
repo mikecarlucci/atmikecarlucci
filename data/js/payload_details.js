b4a2c3d6b7795f6b47537ace42392c2860752d59
Mike Carlucci <mike@mikecarlucci.org>
2019-04-24 23:10:21 -0400

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20361,
-	"created_at": "Wed Apr 24 03:10:19 +0000 2019",
+	"tweets": 20363,
+	"created_at": "Thu Apr 25 03:10:20 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,362 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Wed Apr 24 21:19:15 +0000 2019",
+		"id": 1121161704384102400,
+		"id_str": "1121161704384102403",
+		"full_text": "Good dog #PoetryontheT https://t.co/iTdqasqYwU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			22
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "PoetryontheT",
+					"indices": [
+						9,
+						22
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1121161679239372800,
+					"id_str": "1121161679239372807",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/D48qm2rX4AcZylX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D48qm2rX4AcZylX.jpg",
+					"url": "https://t.co/iTdqasqYwU",
+					"display_url": "pic.twitter.com/iTdqasqYwU",
+					"expanded_url": "https://twitter.com/mikecarlucci/status/1121161704384102403/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1121161679239372800,
+					"id_str": "1121161679239372807",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/D48qm2rX4AcZylX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D48qm2rX4AcZylX.jpg",
+					"url": "https://t.co/iTdqasqYwU",
+					"display_url": "pic.twitter.com/iTdqasqYwU",
+					"expanded_url": "https://twitter.com/mikecarlucci/status/1121161704384102403/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 90161583,
+			"id_str": "90161583",
+			"name": "Mike Carlucci",
+			"screen_name": "mikecarlucci",
+			"location": "Boston, MA",
+			"description": "Writing about the Red Sox @OverTheMonster Data Analyst @Northeastern. Co-host @ozminute mike@mikecarlucci.org\n📷mikegcarlucci",
+			"url": "https://t.co/AvA6PiVYL1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/AvA6PiVYL1",
+							"expanded_url": "https://www.sbnation.com/users/mikecarlucci/",
+							"display_url": "sbnation.com/users/mikecarl…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4530,
+			"friends_count": 750,
+			"listed_count": 49,
+			"created_at": "Sun Nov 15 13:32:20 +0000 2009",
+			"favourites_count": 51079,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 20040,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/90161583/1398534968",
+			"profile_link_color": "0A0AFF",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EDE8ED",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Good dog #PoetryontheT https://t.co/iTdqasqYwU"
+	},
+	{
+		"created_at": "Wed Apr 24 16:32:39 +0000 2019",
+		"id": 1121083510570147800,
+		"id_str": "1121089578583064577",
+		"full_text": "The American League’s Food Fest offerings aren’t much better you guys. https://t.co/5k5BUYWn3N",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			70
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1121083442475622400,
+					"id_str": "1121083442475622402",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/D47jc4BU4AISTi8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D47jc4BU4AISTi8.jpg",
+					"url": "https://t.co/5k5BUYWn3N",
+					"display_url": "pic.twitter.com/5k5BUYWn3N",
+					"expanded_url": "https://twitter.com/ProductiveOuts/status/1121083510570147840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 827,
+							"h": 1114,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 827,
+							"h": 1114,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 505,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1121083442475622400,
+					"id_str": "1121083442475622402",
+					"indices": [
+						71,
+						94
+					],
+					"media_url": "http://pbs.twimg.com/media/D47jc4BU4AISTi8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D47jc4BU4AISTi8.jpg",
+					"url": "https://t.co/5k5BUYWn3N",
+					"display_url": "pic.twitter.com/5k5BUYWn3N",
+					"expanded_url": "https://twitter.com/ProductiveOuts/status/1121083510570147840/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 827,
+							"h": 1114,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 827,
+							"h": 1114,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 505,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1120729579209904100,
+		"in_reply_to_status_id_str": "1120729579209904128",
+		"in_reply_to_user_id": 295015467,
+		"in_reply_to_user_id_str": "295015467",
+		"in_reply_to_screen_name": "ProductiveOuts",
+		"user": {
+			"id": 295015467,
+			"id_str": "295015467",
+			"name": "Productive Outs",
+			"screen_name": "ProductiveOuts",
+			"location": "Los Angeles of Anaheim & Oakland",
+			"description": "Two musicians who love the basedballs. Artisanal namecrafters since 2011. Extremely serious. #sticktomusic",
+			"url": "https://t.co/rTEpkVJujj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/rTEpkVJujj",
+							"expanded_url": "http://productiveouts.com",
+							"display_url": "productiveouts.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10914,
+			"friends_count": 202,
+			"listed_count": 481,
+			"created_at": "Sun May 08 06:23:59 +0000 2011",
+			"favourites_count": 20725,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 45417,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "4D4C4D",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1055862121693626369/YOUPkxPv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1055862121693626369/YOUPkxPv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/295015467/1511283192",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 21,
+		"favorite_count": 81,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The American League’s Food Fest offerings aren’t much better you guys. https://t.co/5k5BUYWn3N"
+	},
+	{
 		"created_at": "Wed Apr 24 02:09:30 +0000 2019",
 		"id": 1120872162968129500,
 		"id_str": "1120872362768044033",
@@ -696,7 +1052,7 @@ Grailbird.data.tweets_2019_04 =
 		"created_at": "Tue Apr 23 19:08:29 +0000 2019",
 		"id": 1120766411398365200,
 		"id_str": "1120766411398365185",
-		"full_text": "@danreines @Unlocator I uninstalled the app and now if the message pops up restarting the apple tv seems to fix it. Not the greatest solution but 🤷‍♂️",
+		"full_text": "@danreines @Unlocator I uninstalled the app and now if the message pops up restarting the apple tv seems to fix it. Not the greatest solution but 🤷♂️",
 		"truncated": false,
 		"display_text_range": [
 			22,
@@ -806,7 +1162,7 @@ Grailbird.data.tweets_2019_04 =
 		"favorited": false,
 		"retweeted": false,
 		"lang": "en",
-		"text": "@danreines @Unlocator I uninstalled the app and now if the message pops up restarting the apple tv seems to fix it. Not the greatest solution but 🤷‍♂️"
+		"text": "@danreines @Unlocator I uninstalled the app and now if the message pops up restarting the apple tv seems to fix it. Not the greatest solution but 🤷♂️"
 	},
 	{
 		"created_at": "Tue Apr 23 19:04:51 +0000 2019",
