bcddf22c1ba2044fc1200ab1a9bad8cc6cc642df
Mike Carlucci <mike@mikecarlucci.org>
2019-07-01 23:10:20 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20567,
-	"created_at": "Sun Jun 30 03:10:20 +0000 2019",
+	"tweets": 20568,
+	"created_at": "Tue Jul 02 03:10:19 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_07.js
@@ -0,0 +1,98 @@
+Grailbird.data.tweets_2019_07 = 
+[
+	{
+		"created_at": "Mon Jul 01 14:22:32 +0000 2019",
+		"id": 1145622885878435800,
+		"id_str": "1145699208684605440",
+		"full_text": "Happy MLB Deferment Day!\n\nThis thread is dedicated to all (Bobby Bonilla is certainly not the only one) of the current and future deferred payments given to MLB players.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			169
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 58825119,
+			"id_str": "58825119",
+			"name": "Michael Mayer",
+			"screen_name": "mikemayerMMO",
+			"location": "Maine",
+			"description": "Executive Editor for @MetsMerized and @Mets_Minors. Part of @IBWAA",
+			"url": "http://t.co/pXKGtRRnJH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/pXKGtRRnJH",
+							"expanded_url": "http://Metsmerizedonline.com",
+							"display_url": "Metsmerizedonline.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10283,
+			"friends_count": 1034,
+			"listed_count": 275,
+			"created_at": "Tue Jul 21 15:28:43 +0000 2009",
+			"favourites_count": 90217,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 163401,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1026555842026500096/w_vw86-3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1026555842026500096/w_vw86-3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/58825119/1561745453",
+			"profile_link_color": "FA743E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 479,
+		"favorite_count": 939,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Happy MLB Deferment Day!\n\nThis thread is dedicated to all (Bobby Bonilla is certainly not the only one) of the current and future deferred payments given to MLB players."
+	}
+]
