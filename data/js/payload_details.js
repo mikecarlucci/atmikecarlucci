f11db724cbb553343027484fbc3685247f6d9636
Mike Carlucci <mike@mikecarlucci.org>
2019-09-17 23:10:20 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20794,
-	"created_at": "Tue Sep 17 03:10:19 +0000 2019",
+	"tweets": 20795,
+	"created_at": "Wed Sep 18 03:10:19 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,98 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Tue Sep 17 11:48:11 +0000 2019",
+		"id": 1173923129107914800,
+		"id_str": "1173926620673036288",
+		"full_text": "For those counting, it will be 13,134 days between Yaz appearances in outfield at Fenway Park",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21762851,
+			"id_str": "21762851",
+			"name": "Rob Bradford",
+			"screen_name": "bradfo",
+			"location": "Boston",
+			"description": "I write about the Red Sox for https://t.co/rRivCTjPj1 and host a podcast called 'The Bradfo Sho' ... Good times!",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/rRivCTjPj1",
+							"expanded_url": "http://WEEI.com",
+							"display_url": "WEEI.com",
+							"indices": [
+								30,
+								53
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 63471,
+			"friends_count": 2271,
+			"listed_count": 1908,
+			"created_at": "Tue Feb 24 15:28:55 +0000 2009",
+			"favourites_count": 2931,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 29653,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1164289364488327168/xCw2eVDe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1164289364488327168/xCw2eVDe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21762851/1483147755",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 101,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "For those counting, it will be 13,134 days between Yaz appearances in outfield at Fenway Park"
+	},
+	{
 		"created_at": "Mon Sep 16 23:30:06 +0000 2019",
 		"id": 1173740875563839500,
 		"id_str": "1173740875563839489",
