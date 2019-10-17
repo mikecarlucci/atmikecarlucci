0c88101a1b1a8f32a3c5d972d9cb7e7dcaebaadb
Mike Carlucci <mike@mikecarlucci.org>
2019-10-16 23:10:19 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20843,
-	"created_at": "Wed Oct 16 03:10:19 +0000 2019",
+	"tweets": 20844,
+	"created_at": "Thu Oct 17 03:10:19 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Wed Oct 16 22:14:50 +0000 2019",
+		"id": 1184519466631061500,
+		"id_str": "1184593569367150594",
+		"full_text": "If Fernando Rodney gets in a game for the Nationals in the World Series he will have pitched in the AL Wild Card Game, NL Wild Card Game, ALDS, NLDS, ALCS, NLCS and World Series for both an AL and NL team.\n\nhttps://t.co/T3P7MGtn0C",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			230
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/T3P7MGtn0C",
+					"expanded_url": "https://www.baseball-reference.com/players/r/rodnefe01.shtml#pitching_postseason::none",
+					"display_url": "baseball-reference.com/players/r/rodn…",
+					"indices": [
+						207,
+						230
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 735620016,
+			"id_str": "735620016",
+			"name": "Aidan Jackson-Evans",
+			"screen_name": "ajacksonevans",
+			"location": "",
+			"description": "Baseball writer and analyst for @HighHeatStats. Written for USA Today Sports Weekly. I tweet baseball stats.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2822,
+			"friends_count": 415,
+			"listed_count": 91,
+			"created_at": "Fri Aug 03 22:07:30 +0000 2012",
+			"favourites_count": 3506,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10700,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/578393677866860544/ILm7wNsT_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/578393677866860544/ILm7wNsT_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/735620016/1426734737",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 301,
+		"favorite_count": 1498,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "If Fernando Rodney gets in a game for the Nationals in the World Series he will have pitched in the AL Wild Card Game, NL Wild Card Game, ALDS, NLDS, ALCS, NLCS and World Series for both an AL and NL team.\n\nhttps://t.co/T3P7MGtn0C"
+	},
+	{
 		"created_at": "Tue Oct 15 19:04:29 +0000 2019",
 		"id": 1184183278376407000,
 		"id_str": "1184183278376407040",
