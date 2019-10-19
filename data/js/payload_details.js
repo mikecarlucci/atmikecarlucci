7f6df3014f45ba2ba72518b19436a916751854ab
Mike Carlucci <mike@mikecarlucci.org>
2019-10-18 23:10:20 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20845,
-	"created_at": "Fri Oct 18 03:10:19 +0000 2019",
+	"tweets": 20846,
+	"created_at": "Sat Oct 19 03:10:19 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Fri Oct 18 22:40:34 +0000 2019",
+		"id": 1185321682829041700,
+		"id_str": "1185324821346955264",
+		"full_text": "Game five of the 2004 ALCS began 15 years ago tonight and ended with the immortal words “AND HE CAN KEEP ON RUNNING TO NEW YORK. GAME SIX TOMORROW NIGHT.” Still get chills and I’ve watched it 20 million times (or close to that). https://t.co/t5yyFWSxE9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			228
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1185321466570694700,
+					"id_str": "1185321466570694656",
+					"indices": [
+						229,
+						252
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1185321466570694656/pu/img/t9G2V1tS921Cdn6p.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1185321466570694656/pu/img/t9G2V1tS921Cdn6p.jpg",
+					"url": "https://t.co/t5yyFWSxE9",
+					"display_url": "pic.twitter.com/t5yyFWSxE9",
+					"expanded_url": "https://twitter.com/SurvivingGrady/status/1185321682829041665/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1185321466570694700,
+					"id_str": "1185321466570694656",
+					"indices": [
+						229,
+						252
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1185321466570694656/pu/img/t9G2V1tS921Cdn6p.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1185321466570694656/pu/img/t9G2V1tS921Cdn6p.jpg",
+					"url": "https://t.co/t5yyFWSxE9",
+					"display_url": "pic.twitter.com/t5yyFWSxE9",
+					"expanded_url": "https://twitter.com/SurvivingGrady/status/1185321682829041665/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 28803,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1185321466570694656/pu/vid/1280x720/AfN-H-X2WlWr15wr.mp4?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1185321466570694656/pu/vid/480x270/INEKYGnaXro5mo3L.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1185321466570694656/pu/pl/46_9mAXDXtbM9NIS.m3u8?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1185321466570694656/pu/vid/640x360/cxASNxtQVixe62JW.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 22878740,
+			"id_str": "22878740",
+			"name": "Red",
+			"screen_name": "SurvivingGrady",
+			"location": "West to the Rox, MA",
+			"description": "Red Sox blogger. Freelance drinker. Comic book reader. Pop culture enthusiast. Butch Hobson apologist. 100% felt. Insta: survivinggrady | red@survivinggrady.com",
+			"url": "https://t.co/yTogiN6HFD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/yTogiN6HFD",
+							"expanded_url": "http://www.survivinggrady.com",
+							"display_url": "survivinggrady.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18883,
+			"friends_count": 2091,
+			"listed_count": 638,
+			"created_at": "Thu Mar 05 04:16:44 +0000 2009",
+			"favourites_count": 134576,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 43602,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/616803292941676544/LlcK7eD1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/616803292941676544/LlcK7eD1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/22878740/1523840622",
+			"profile_link_color": "088253",
+			"profile_sidebar_border_color": "D3D2CF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "634047",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 198,
+		"favorite_count": 895,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Game five of the 2004 ALCS began 15 years ago tonight and ended with the immortal words “AND HE CAN KEEP ON RUNNING TO NEW YORK. GAME SIX TOMORROW NIGHT.” Still get chills and I’ve watched it 20 million times (or close to that). https://t.co/t5yyFWSxE9"
+	},
+	{
 		"created_at": "Fri Oct 18 02:49:40 +0000 2019",
 		"id": 1185019653560647700,
 		"id_str": "1185025123566477314",
