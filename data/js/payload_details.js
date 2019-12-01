3547e433f1e7918acb9a9acd3e551f4cce8e72f6
Mike Carlucci <mike@mikecarlucci.org>
2019-11-30 22:10:20 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20917,
-	"created_at": "Fri Nov 29 03:10:19 +0000 2019",
+	"tweets": 20918,
+	"created_at": "Sun Dec 01 03:10:19 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_12.js
@@ -0,0 +1,657 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Sun Dec 01 01:18:17 +0000 2019",
+		"id": 1200947190216249300,
+		"id_str": "1200947190216249344",
+		"full_text": "Get excited https://t.co/YJPGcRqSQB https://t.co/diCyBNgk74",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			35
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YJPGcRqSQB",
+					"expanded_url": "https://twitter.com/nextscenepod/status/1200945101100535811",
+					"display_url": "twitter.com/nextscenepod/s…",
+					"indices": [
+						12,
+						35
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1200947175506751500,
+					"id_str": "1200947175506751491",
+					"indices": [
+						36,
+						59
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EKqfF94WsAMVsU6.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EKqfF94WsAMVsU6.jpg",
+					"url": "https://t.co/diCyBNgk74",
+					"display_url": "pic.twitter.com/diCyBNgk74",
+					"expanded_url": "https://twitter.com/mikecarlucci/status/1200947190216249344/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 500,
+							"h": 268,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 268,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 268,
+							"resize": "fit"
+						}
+					},
+					"features": {}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1200947175506751500,
+					"id_str": "1200947175506751491",
+					"indices": [
+						36,
+						59
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EKqfF94WsAMVsU6.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EKqfF94WsAMVsU6.jpg",
+					"url": "https://t.co/diCyBNgk74",
+					"display_url": "pic.twitter.com/diCyBNgk74",
+					"expanded_url": "https://twitter.com/mikecarlucci/status/1200947190216249344/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 500,
+							"h": 268,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 268,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 500,
+							"h": 268,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							125,
+							67
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EKqfF94WsAMVsU6.mp4"
+							}
+						]
+					},
+					"features": {}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 90161583,
+			"id_str": "90161583",
+			"name": "Mike Carlucci",
+			"screen_name": "mikecarlucci",
+			"location": "Boston, MA",
+			"description": "Writing about the Red Sox @OverTheMonster Data Analyst @Northeastern. Co-host @ozminute mike@mikecarlucci.org\n📷mikegcarlucci",
+			"url": "https://t.co/AvA6PiVYL1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/AvA6PiVYL1",
+							"expanded_url": "https://www.sbnation.com/users/mikecarlucci/",
+							"display_url": "sbnation.com/users/mikecarl…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4508,
+			"friends_count": 721,
+			"listed_count": 48,
+			"created_at": "Sun Nov 15 13:32:20 +0000 2009",
+			"favourites_count": 58327,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17463,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/90161583/1398534968",
+			"profile_link_color": "0A0AFF",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EDE8ED",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1200945101100535800,
+		"quoted_status_id_str": "1200945101100535811",
+		"quoted_status_permalink": {
+			"url": "https://t.co/YJPGcRqSQB",
+			"expanded": "https://twitter.com/nextscenepod/status/1200945101100535811",
+			"display": "twitter.com/nextscenepod/s…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Dec 01 01:09:59 +0000 2019",
+			"id": 1200945101100535800,
+			"id_str": "1200945101100535811",
+			"full_text": "Rachel Mummert @DCGreenberg @mikecarlucci @somnambulyst are here to execute part 6. #podernfamily @mcmnky @bbb95 @Tagard_McStone @5MinutesMystery @IndianaJonesMin @NLCVacationDays  @RockyMinute  @ozminute @homealoneminute https://t.co/eDiArs8jJl https://t.co/hZQbjZHm8l",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				245
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "podernfamily",
+						"indices": [
+							84,
+							97
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "DCGreenberg",
+						"name": "Doug Greenberg",
+						"id": 300593524,
+						"id_str": "300593524",
+						"indices": [
+							15,
+							27
+						]
+					},
+					{
+						"screen_name": "mikecarlucci",
+						"name": "Mike Carlucci",
+						"id": 90161583,
+						"id_str": "90161583",
+						"indices": [
+							28,
+							41
+						]
+					},
+					{
+						"screen_name": "somnambulyst",
+						"name": "Pete Mummert",
+						"id": 563164867,
+						"id_str": "563164867",
+						"indices": [
+							42,
+							55
+						]
+					},
+					{
+						"screen_name": "mcmnky",
+						"name": "Sean German",
+						"id": 345574730,
+						"id_str": "345574730",
+						"indices": [
+							98,
+							105
+						]
+					},
+					{
+						"screen_name": "bbb95",
+						"name": "BG",
+						"id": 74615836,
+						"id_str": "74615836",
+						"indices": [
+							106,
+							112
+						]
+					},
+					{
+						"screen_name": "Tagard_McStone",
+						"name": "Dave Pallas",
+						"id": 68321268,
+						"id_str": "68321268",
+						"indices": [
+							113,
+							128
+						]
+					},
+					{
+						"screen_name": "5MinutesMystery",
+						"name": "5 Minutes Of Mystery",
+						"id": 866137281043849200,
+						"id_str": "866137281043849217",
+						"indices": [
+							129,
+							145
+						]
+					},
+					{
+						"screen_name": "IndianaJonesMin",
+						"name": "Indiana Jones Minute",
+						"id": 708864606920269800,
+						"id_str": "708864606920269824",
+						"indices": [
+							146,
+							162
+						]
+					},
+					{
+						"screen_name": "NLCVacationDays",
+						"name": "NLC VacationDays",
+						"id": 824996314912006100,
+						"id_str": "824996314912006144",
+						"indices": [
+							163,
+							179
+						]
+					},
+					{
+						"screen_name": "RockyMinute",
+						"name": "Rocky Minute",
+						"id": 893158598137827300,
+						"id_str": "893158598137827328",
+						"indices": [
+							181,
+							193
+						]
+					},
+					{
+						"screen_name": "ozminute",
+						"name": "Return to Oz Minute",
+						"id": 805783012961124400,
+						"id_str": "805783012961124352",
+						"indices": [
+							195,
+							204
+						]
+					},
+					{
+						"screen_name": "homealoneminute",
+						"name": "Home Alone Minute",
+						"id": 1064590382741286900,
+						"id_str": "1064590382741286912",
+						"indices": [
+							205,
+							221
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/eDiArs8jJl",
+						"expanded_url": "https://nextscenepod.com/podcast/home-alone-for-the-holidays-part-6/",
+						"display_url": "nextscenepod.com/podcast/home-a…",
+						"indices": [
+							222,
+							245
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1200945098466504700,
+						"id_str": "1200945098466504706",
+						"indices": [
+							246,
+							269
+						],
+						"media_url": "http://pbs.twimg.com/media/EKqdNETXsAIdgGr.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EKqdNETXsAIdgGr.jpg",
+						"url": "https://t.co/hZQbjZHm8l",
+						"display_url": "pic.twitter.com/hZQbjZHm8l",
+						"expanded_url": "https://twitter.com/NextScenePod/status/1200945101100535811/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 654,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1322,
+								"h": 720,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 370,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"medium": {
+								"faces": [
+									{
+										"x": 855,
+										"y": 66,
+										"h": 103,
+										"w": 103
+									},
+									{
+										"x": 424,
+										"y": 115,
+										"h": 296,
+										"w": 296
+									}
+								]
+							},
+							"orig": {
+								"faces": [
+									{
+										"x": 942,
+										"y": 73,
+										"h": 114,
+										"w": 114
+									},
+									{
+										"x": 468,
+										"y": 127,
+										"h": 327,
+										"w": 327
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 942,
+										"y": 73,
+										"h": 114,
+										"w": 114
+									},
+									{
+										"x": 468,
+										"y": 127,
+										"h": 327,
+										"w": 327
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 484,
+										"y": 37,
+										"h": 58,
+										"w": 58
+									},
+									{
+										"x": 240,
+										"y": 65,
+										"h": 168,
+										"w": 168
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1200945098466504700,
+						"id_str": "1200945098466504706",
+						"indices": [
+							246,
+							269
+						],
+						"media_url": "http://pbs.twimg.com/media/EKqdNETXsAIdgGr.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EKqdNETXsAIdgGr.jpg",
+						"url": "https://t.co/hZQbjZHm8l",
+						"display_url": "pic.twitter.com/hZQbjZHm8l",
+						"expanded_url": "https://twitter.com/NextScenePod/status/1200945101100535811/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 654,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1322,
+								"h": 720,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 370,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"medium": {
+								"faces": [
+									{
+										"x": 855,
+										"y": 66,
+										"h": 103,
+										"w": 103
+									},
+									{
+										"x": 424,
+										"y": 115,
+										"h": 296,
+										"w": 296
+									}
+								]
+							},
+							"orig": {
+								"faces": [
+									{
+										"x": 942,
+										"y": 73,
+										"h": 114,
+										"w": 114
+									},
+									{
+										"x": 468,
+										"y": 127,
+										"h": 327,
+										"w": 327
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 942,
+										"y": 73,
+										"h": 114,
+										"w": 114
+									},
+									{
+										"x": 468,
+										"y": 127,
+										"h": 327,
+										"w": 327
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 484,
+										"y": 37,
+										"h": 58,
+										"w": 58
+									},
+									{
+										"x": 240,
+										"y": 65,
+										"h": 168,
+										"w": 168
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://www.blog2social.com\" rel=\"nofollow\">Blog2Social APP</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1160032968573968400,
+				"id_str": "1160032968573968384",
+				"name": "The Next Scene",
+				"screen_name": "NextScenePod",
+				"location": "",
+				"description": "A podcast taking on pop culure one scene at a time",
+				"url": "https://t.co/hIv3wcdIiL",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/hIv3wcdIiL",
+								"expanded_url": "https://nextscenepod.com/",
+								"display_url": "nextscenepod.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 42,
+				"friends_count": 91,
+				"listed_count": 0,
+				"created_at": "Sat Aug 10 03:39:46 +0000 2019",
+				"favourites_count": 21,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 40,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1181738710221627401/wW4PSi3x_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1181738710221627401/wW4PSi3x_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1160032968573968384/1570583440",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 6,
+			"favorite_count": 4,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Get excited https://t.co/YJPGcRqSQB https://t.co/diCyBNgk74"
+	}
+]
