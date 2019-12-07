9fbc105e2ac3862c902707a05010d0e3cc66342c
Mike Carlucci <mike@mikecarlucci.org>
2019-12-06 22:10:20 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 20926,
-	"created_at": "Fri Dec 06 03:10:19 +0000 2019",
+	"tweets": 20927,
+	"created_at": "Sat Dec 07 03:10:19 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,917 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Fri Dec 06 20:44:15 +0000 2019",
+		"id": 1203052555745386500,
+		"id_str": "1203052555745386496",
+		"full_text": "https://t.co/AMzRqrXP4H https://t.co/VEq6FTrOsU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			23
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/AMzRqrXP4H",
+					"expanded_url": "https://twitter.com/SBNation/status/1202963910330068992",
+					"display_url": "twitter.com/SBNation/statu…",
+					"indices": [
+						0,
+						23
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1203052550494118000,
+					"id_str": "1203052550494117888",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/ELIZ665UYAAOl3s.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELIZ665UYAAOl3s.jpg",
+					"url": "https://t.co/VEq6FTrOsU",
+					"display_url": "pic.twitter.com/VEq6FTrOsU",
+					"expanded_url": "https://twitter.com/mikecarlucci/status/1203052555745386496/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 541,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 814,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 814,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 557,
+									"y": 13,
+									"h": 28,
+									"w": 28
+								},
+								{
+									"x": 114,
+									"y": 23,
+									"h": 35,
+									"w": 35
+								},
+								{
+									"x": 426,
+									"y": 35,
+									"h": 25,
+									"w": 25
+								},
+								{
+									"x": 316,
+									"y": 59,
+									"h": 25,
+									"w": 25
+								},
+								{
+									"x": 531,
+									"y": 73,
+									"h": 28,
+									"w": 28
+								},
+								{
+									"x": 425,
+									"y": 106,
+									"h": 26,
+									"w": 26
+								},
+								{
+									"x": 122,
+									"y": 139,
+									"h": 26,
+									"w": 26
+								},
+								{
+									"x": 52,
+									"y": 5,
+									"h": 29,
+									"w": 29
+								},
+								{
+									"x": 223,
+									"y": 43,
+									"h": 28,
+									"w": 28
+								},
+								{
+									"x": 332,
+									"y": 155,
+									"h": 26,
+									"w": 26
+								},
+								{
+									"x": 17,
+									"y": 49,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 214,
+									"y": 54,
+									"h": 46,
+									"w": 46
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 840,
+									"y": 21,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 173,
+									"y": 36,
+									"h": 54,
+									"w": 54
+								},
+								{
+									"x": 643,
+									"y": 53,
+									"h": 38,
+									"w": 38
+								},
+								{
+									"x": 476,
+									"y": 90,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 801,
+									"y": 111,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 640,
+									"y": 161,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 184,
+									"y": 210,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 79,
+									"y": 8,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 336,
+									"y": 65,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 500,
+									"y": 234,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 26,
+									"y": 74,
+									"h": 55,
+									"w": 55
+								},
+								{
+									"x": 323,
+									"y": 82,
+									"h": 70,
+									"w": 70
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 840,
+									"y": 21,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 173,
+									"y": 36,
+									"h": 54,
+									"w": 54
+								},
+								{
+									"x": 643,
+									"y": 53,
+									"h": 38,
+									"w": 38
+								},
+								{
+									"x": 476,
+									"y": 90,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 801,
+									"y": 111,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 640,
+									"y": 161,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 184,
+									"y": 210,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 79,
+									"y": 8,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 336,
+									"y": 65,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 500,
+									"y": 234,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 26,
+									"y": 74,
+									"h": 55,
+									"w": 55
+								},
+								{
+									"x": 323,
+									"y": 82,
+									"h": 70,
+									"w": 70
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 840,
+									"y": 21,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 173,
+									"y": 36,
+									"h": 54,
+									"w": 54
+								},
+								{
+									"x": 643,
+									"y": 53,
+									"h": 38,
+									"w": 38
+								},
+								{
+									"x": 476,
+									"y": 90,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 801,
+									"y": 111,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 640,
+									"y": 161,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 184,
+									"y": 210,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 79,
+									"y": 8,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 336,
+									"y": 65,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 500,
+									"y": 234,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 26,
+									"y": 74,
+									"h": 55,
+									"w": 55
+								},
+								{
+									"x": 323,
+									"y": 82,
+									"h": 70,
+									"w": 70
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1203052550494118000,
+					"id_str": "1203052550494117888",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/ELIZ665UYAAOl3s.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELIZ665UYAAOl3s.jpg",
+					"url": "https://t.co/VEq6FTrOsU",
+					"display_url": "pic.twitter.com/VEq6FTrOsU",
+					"expanded_url": "https://twitter.com/mikecarlucci/status/1203052555745386496/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 541,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 814,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 814,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 557,
+									"y": 13,
+									"h": 28,
+									"w": 28
+								},
+								{
+									"x": 114,
+									"y": 23,
+									"h": 35,
+									"w": 35
+								},
+								{
+									"x": 426,
+									"y": 35,
+									"h": 25,
+									"w": 25
+								},
+								{
+									"x": 316,
+									"y": 59,
+									"h": 25,
+									"w": 25
+								},
+								{
+									"x": 531,
+									"y": 73,
+									"h": 28,
+									"w": 28
+								},
+								{
+									"x": 425,
+									"y": 106,
+									"h": 26,
+									"w": 26
+								},
+								{
+									"x": 122,
+									"y": 139,
+									"h": 26,
+									"w": 26
+								},
+								{
+									"x": 52,
+									"y": 5,
+									"h": 29,
+									"w": 29
+								},
+								{
+									"x": 223,
+									"y": 43,
+									"h": 28,
+									"w": 28
+								},
+								{
+									"x": 332,
+									"y": 155,
+									"h": 26,
+									"w": 26
+								},
+								{
+									"x": 17,
+									"y": 49,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 214,
+									"y": 54,
+									"h": 46,
+									"w": 46
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 840,
+									"y": 21,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 173,
+									"y": 36,
+									"h": 54,
+									"w": 54
+								},
+								{
+									"x": 643,
+									"y": 53,
+									"h": 38,
+									"w": 38
+								},
+								{
+									"x": 476,
+									"y": 90,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 801,
+									"y": 111,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 640,
+									"y": 161,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 184,
+									"y": 210,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 79,
+									"y": 8,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 336,
+									"y": 65,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 500,
+									"y": 234,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 26,
+									"y": 74,
+									"h": 55,
+									"w": 55
+								},
+								{
+									"x": 323,
+									"y": 82,
+									"h": 70,
+									"w": 70
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 840,
+									"y": 21,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 173,
+									"y": 36,
+									"h": 54,
+									"w": 54
+								},
+								{
+									"x": 643,
+									"y": 53,
+									"h": 38,
+									"w": 38
+								},
+								{
+									"x": 476,
+									"y": 90,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 801,
+									"y": 111,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 640,
+									"y": 161,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 184,
+									"y": 210,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 79,
+									"y": 8,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 336,
+									"y": 65,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 500,
+									"y": 234,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 26,
+									"y": 74,
+									"h": 55,
+									"w": 55
+								},
+								{
+									"x": 323,
+									"y": 82,
+									"h": 70,
+									"w": 70
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 840,
+									"y": 21,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 173,
+									"y": 36,
+									"h": 54,
+									"w": 54
+								},
+								{
+									"x": 643,
+									"y": 53,
+									"h": 38,
+									"w": 38
+								},
+								{
+									"x": 476,
+									"y": 90,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 801,
+									"y": 111,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 640,
+									"y": 161,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 184,
+									"y": 210,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 79,
+									"y": 8,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 336,
+									"y": 65,
+									"h": 43,
+									"w": 43
+								},
+								{
+									"x": 500,
+									"y": 234,
+									"h": 40,
+									"w": 40
+								},
+								{
+									"x": 26,
+									"y": 74,
+									"h": 55,
+									"w": 55
+								},
+								{
+									"x": 323,
+									"y": 82,
+									"h": 70,
+									"w": 70
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 90161583,
+			"id_str": "90161583",
+			"name": "Mike Carlucci",
+			"screen_name": "mikecarlucci",
+			"location": "Boston, MA",
+			"description": "Writing about the Red Sox @OverTheMonster Data Analyst @Northeastern. Co-host @ozminute mike@mikecarlucci.org\n📷mikegcarlucci",
+			"url": "https://t.co/AvA6PiVYL1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/AvA6PiVYL1",
+							"expanded_url": "https://www.sbnation.com/users/mikecarlucci/",
+							"display_url": "sbnation.com/users/mikecarl…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4513,
+			"friends_count": 721,
+			"listed_count": 48,
+			"created_at": "Sun Nov 15 13:32:20 +0000 2009",
+			"favourites_count": 58534,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17466,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/90161583/1398534968",
+			"profile_link_color": "0A0AFF",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EDE8ED",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1202963910330069000,
+		"quoted_status_id_str": "1202963910330068992",
+		"quoted_status_permalink": {
+			"url": "https://t.co/AMzRqrXP4H",
+			"expanded": "https://twitter.com/SBNation/status/1202963910330068992",
+			"display": "twitter.com/SBNation/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Dec 06 14:52:00 +0000 2019",
+			"id": 1202963910330069000,
+			"id_str": "1202963910330068992",
+			"full_text": "what's your favorite sports photo of the decade?",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				48
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16745015,
+				"id_str": "16745015",
+				"name": "SB Nation",
+				"screen_name": "SBNation",
+				"location": "",
+				"description": "watch Rewinder, Collapse, and Untitled at https://t.co/FXqGNlphxN",
+				"url": "https://t.co/mlylfVPR3G",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/mlylfVPR3G",
+								"expanded_url": "http://sbnation.com",
+								"display_url": "sbnation.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/FXqGNlphxN",
+								"expanded_url": "http://youtube.com/sbnation",
+								"display_url": "youtube.com/sbnation",
+								"indices": [
+									42,
+									65
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 313726,
+				"friends_count": 989,
+				"listed_count": 5469,
+				"created_at": "Tue Oct 14 20:17:11 +0000 2008",
+				"favourites_count": 8249,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 290621,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "BBB8B7",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1144296456939016193/ztr7g6Iy_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1144296456939016193/ztr7g6Iy_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16745015/1573491312",
+				"profile_link_color": "C52126",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "E4E4E4",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 198,
+			"favorite_count": 1649,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/AMzRqrXP4H https://t.co/VEq6FTrOsU"
+	},
+	{
 		"created_at": "Thu Dec 05 20:46:02 +0000 2019",
 		"id": 1202690615701463000,
 		"id_str": "1202690615701463041",
