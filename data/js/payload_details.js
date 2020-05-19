88844a4aaa955709888373b52298385c4ec9e199
Mike Carlucci <mike@mikecarlucci.org>
2020-05-18 23:10:23 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 21577,
-	"created_at": "Mon May 18 03:10:20 +0000 2020",
+	"tweets": 21578,
+	"created_at": "Tue May 19 03:10:21 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Mon May 18 23:50:43 +0000 2020",
+		"id": 1262530932990337000,
+		"id_str": "1262531090801098752",
+		"full_text": "My plans.                          2020. https://t.co/bRrqJcBSIr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			40
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1262530925759357000,
+					"id_str": "1262530925759356929",
+					"indices": [
+						41,
+						64
+					],
+					"media_url": "http://pbs.twimg.com/media/EYVpMBPU4AEuapy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EYVpMBPU4AEuapy.jpg",
+					"url": "https://t.co/bRrqJcBSIr",
+					"display_url": "pic.twitter.com/bRrqJcBSIr",
+					"expanded_url": "https://twitter.com/DeathStarPR/status/1262530932990337024/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 567,
+							"h": 541,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 567,
+							"h": 541,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 567,
+							"h": 541,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1262530925759357000,
+					"id_str": "1262530925759356929",
+					"indices": [
+						41,
+						64
+					],
+					"media_url": "http://pbs.twimg.com/media/EYVpMBPU4AEuapy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EYVpMBPU4AEuapy.jpg",
+					"url": "https://t.co/bRrqJcBSIr",
+					"display_url": "pic.twitter.com/bRrqJcBSIr",
+					"expanded_url": "https://twitter.com/DeathStarPR/status/1262530932990337024/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 567,
+							"h": 541,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 567,
+							"h": 541,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 567,
+							"h": 541,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1262530929970475000,
+					"id_str": "1262530929970475008",
+					"indices": [
+						41,
+						64
+					],
+					"media_url": "http://pbs.twimg.com/media/EYVpMQ7VcAA551E.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EYVpMQ7VcAA551E.jpg",
+					"url": "https://t.co/bRrqJcBSIr",
+					"display_url": "pic.twitter.com/bRrqJcBSIr",
+					"expanded_url": "https://twitter.com/DeathStarPR/status/1262530932990337024/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 765,
+							"h": 401,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 765,
+							"h": 401,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 356,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 157218534,
+			"id_str": "157218534",
+			"name": "Death Star PR",
+			"screen_name": "DeathStarPR",
+			"location": "In a galaxy far, far away...",
+			"description": "The Galactic Empire has been getting a bad rap. We're here to set the record straight. Email: DeathStarPR@gmail.com",
+			"url": "https://t.co/CfcdL2zwfE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CfcdL2zwfE",
+							"expanded_url": "http://shop.spreadshirt.com/DeathStarPR/",
+							"display_url": "shop.spreadshirt.com/DeathStarPR/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 319529,
+			"friends_count": 298,
+			"listed_count": 5757,
+			"created_at": "Sat Jun 19 05:14:58 +0000 2010",
+			"favourites_count": 20349,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5835,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "080101",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3661409295/bb35447683362ca080bd39f057a7216c_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3661409295/bb35447683362ca080bd39f057a7216c_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/157218534/1368598512",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 404,
+		"favorite_count": 1377,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "My plans.                          2020. https://t.co/bRrqJcBSIr"
+	},
+	{
 		"created_at": "Sun May 17 16:46:08 +0000 2020",
 		"id": 1262061851476725800,
 		"id_str": "1262061851476725760",
