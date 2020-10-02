55624fa634e9f85e4e6ea0922ba043e79ac09344
Mike Carlucci <mike@mikecarlucci.org>
2020-10-01 23:10:22 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22087,
-	"created_at": "Thu Oct 01 03:10:21 +0000 2020",
+	"tweets": 22088,
+	"created_at": "Fri Oct 02 03:10:20 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_10.js
@@ -0,0 +1,414 @@
+Grailbird.data.tweets_2020_10 = 
+[
+	{
+		"created_at": "Thu Oct 01 19:19:22 +0000 2020",
+		"id": 1311747553659105300,
+		"id_str": "1311747553659105281",
+		"full_text": "Same https://t.co/qx74kGvFpq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			4
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/qx74kGvFpq",
+					"expanded_url": "https://twitter.com/petetheretailer/status/1311727262824165378",
+					"display_url": "twitter.com/petetheretaile…",
+					"indices": [
+						5,
+						28
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 90161583,
+			"id_str": "90161583",
+			"name": "Mike Carlucci",
+			"screen_name": "mikecarlucci",
+			"location": "Boston, MA",
+			"description": "Writing about the Red Sox @OverTheMonster Data Analyst @Northeastern. Co-host @ozminute mike@mikecarlucci.org\n📷mikegcarlucci",
+			"url": "https://t.co/AvA6PiVYL1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/AvA6PiVYL1",
+							"expanded_url": "https://www.sbnation.com/users/mikecarlucci/",
+							"display_url": "sbnation.com/users/mikecarl…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4454,
+			"friends_count": 744,
+			"listed_count": 45,
+			"created_at": "Sun Nov 15 13:32:20 +0000 2009",
+			"favourites_count": 79341,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17483,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/90161583/1590433607",
+			"profile_link_color": "0A0AFF",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EDE8ED",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1311727262824165400,
+		"quoted_status_id_str": "1311727262824165378",
+		"quoted_status_permalink": {
+			"url": "https://t.co/qx74kGvFpq",
+			"expanded": "https://twitter.com/petetheretailer/status/1311727262824165378",
+			"display": "twitter.com/petetheretaile…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Oct 01 17:58:44 +0000 2020",
+			"id": 1311727262824165400,
+			"id_str": "1311727262824165378",
+			"full_text": "https://t.co/E3Y7EGkE6u https://t.co/AXWraEqd6V",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				23
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/E3Y7EGkE6u",
+						"expanded_url": "https://twitter.com/Roberto_Draws/status/1311358347589844995",
+						"display_url": "twitter.com/Roberto_Draws/…",
+						"indices": [
+							0,
+							23
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1311727204691132400,
+						"id_str": "1311727204691132417",
+						"indices": [
+							24,
+							47
+						],
+						"media_url": "http://pbs.twimg.com/media/EjQw8V3UwAEuESa.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EjQw8V3UwAEuESa.jpg",
+						"url": "https://t.co/AXWraEqd6V",
+						"display_url": "pic.twitter.com/AXWraEqd6V",
+						"expanded_url": "https://twitter.com/PeteTheRetailer/status/1311727262824165378/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 289,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 511,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1918,
+								"h": 816,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1311727204691132400,
+						"id_str": "1311727204691132417",
+						"indices": [
+							24,
+							47
+						],
+						"media_url": "http://pbs.twimg.com/media/EjQw8V3UwAEuESa.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EjQw8V3UwAEuESa.jpg",
+						"url": "https://t.co/AXWraEqd6V",
+						"display_url": "pic.twitter.com/AXWraEqd6V",
+						"expanded_url": "https://twitter.com/PeteTheRetailer/status/1311727262824165378/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 289,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 511,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1918,
+								"h": 816,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1311727204707922000,
+						"id_str": "1311727204707921920",
+						"indices": [
+							24,
+							47
+						],
+						"media_url": "http://pbs.twimg.com/media/EjQw8V7U8AARrCd.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EjQw8V7U8AARrCd.jpg",
+						"url": "https://t.co/AXWraEqd6V",
+						"display_url": "pic.twitter.com/AXWraEqd6V",
+						"expanded_url": "https://twitter.com/PeteTheRetailer/status/1311727262824165378/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 700,
+								"h": 470,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 700,
+								"h": 470,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 457,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1311727204779192300,
+						"id_str": "1311727204779192320",
+						"indices": [
+							24,
+							47
+						],
+						"media_url": "http://pbs.twimg.com/media/EjQw8WMUcAA26a2.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EjQw8WMUcAA26a2.jpg",
+						"url": "https://t.co/AXWraEqd6V",
+						"display_url": "pic.twitter.com/AXWraEqd6V",
+						"expanded_url": "https://twitter.com/PeteTheRetailer/status/1311727262824165378/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 497,
+								"h": 261,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 497,
+								"h": 261,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 497,
+								"h": 261,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1311727204926066700,
+						"id_str": "1311727204926066689",
+						"indices": [
+							24,
+							47
+						],
+						"media_url": "http://pbs.twimg.com/media/EjQw8WvVkAEtQkU.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EjQw8WvVkAEtQkU.jpg",
+						"url": "https://t.co/AXWraEqd6V",
+						"display_url": "pic.twitter.com/AXWraEqd6V",
+						"expanded_url": "https://twitter.com/PeteTheRetailer/status/1311727262824165378/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 800,
+								"h": 448,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 800,
+								"h": 448,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 381,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2366030324,
+				"id_str": "2366030324",
+				"name": "Pete The Retailer 🏡",
+				"screen_name": "PeteTheRetailer",
+				"location": "Pasadena, CA",
+				"description": "Repeat Eel Hitter. Co-host/producer of @StarWarsMinute, @ABCDEVO, @alphabeatles and more. He/Him",
+				"url": "https://t.co/78stLaJ88E",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/78stLaJ88E",
+								"expanded_url": "http://www.starwarsminute.com",
+								"display_url": "starwarsminute.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2570,
+				"friends_count": 1141,
+				"listed_count": 23,
+				"created_at": "Fri Feb 28 19:26:46 +0000 2014",
+				"favourites_count": 12365,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 3888,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1268241144657219584/1cnqEtb6_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1268241144657219584/1cnqEtb6_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2366030324/1572541385",
+				"profile_link_color": "314159",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1311358347589845000,
+			"quoted_status_id_str": "1311358347589844995",
+			"quoted_status_permalink": {
+				"url": "https://t.co/E3Y7EGkE6u",
+				"expanded": "https://twitter.com/Roberto_Draws/status/1311358347589844995",
+				"display": "twitter.com/Roberto_Draws/…"
+			},
+			"retweet_count": 2,
+			"favorite_count": 43,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "und"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Same https://t.co/qx74kGvFpq"
+	}
+]
