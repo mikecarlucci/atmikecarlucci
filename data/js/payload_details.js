e389848c467bf126a60321779507595c10569b81
Mike Carlucci <mike@mikecarlucci.org>
2020-11-04 22:10:20 -0500

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22234,
-	"created_at": "Wed Nov 04 03:10:19 +0000 2020",
+	"tweets": 22236,
+	"created_at": "Thu Nov 05 03:10:19 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,504 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Thu Nov 05 00:59:35 +0000 2020",
+		"id": 1324154358142013400,
+		"id_str": "1324154358142013440",
+		"full_text": "This is the only time a cat has ever crossed over into “acceptable” for me. This is the good cat. https://t.co/N9kwMG8IDZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/N9kwMG8IDZ",
+					"expanded_url": "https://twitter.com/ziyatong/status/1324085091329593345",
+					"display_url": "twitter.com/ziyatong/statu…",
+					"indices": [
+						98,
+						121
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 90161583,
+			"id_str": "90161583",
+			"name": "Mike Carlucci",
+			"screen_name": "mikecarlucci",
+			"location": "Boston, MA",
+			"description": "Writing about the Red Sox @OverTheMonster Data Analyst @Northeastern. Co-host @ozminute mike@mikecarlucci.org\n📷mikegcarlucci",
+			"url": "https://t.co/AvA6PiVYL1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/AvA6PiVYL1",
+							"expanded_url": "https://www.sbnation.com/users/mikecarlucci/",
+							"display_url": "sbnation.com/users/mikecarl…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4452,
+			"friends_count": 755,
+			"listed_count": 46,
+			"created_at": "Sun Nov 15 13:32:20 +0000 2009",
+			"favourites_count": 82802,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17556,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/90161583/1590433607",
+			"profile_link_color": "0A0AFF",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EDE8ED",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1324085091329593300,
+		"quoted_status_id_str": "1324085091329593345",
+		"quoted_status_permalink": {
+			"url": "https://t.co/N9kwMG8IDZ",
+			"expanded": "https://twitter.com/ziyatong/status/1324085091329593345",
+			"display": "twitter.com/ziyatong/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Nov 04 20:24:20 +0000 2020",
+			"id": 1324085091329593300,
+			"id_str": "1324085091329593345",
+			"full_text": "🎯🎯🎯 https://t.co/k6VMEYJaTw",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				27
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1324002570663329800,
+						"id_str": "1324002570663329797",
+						"indices": [
+							4,
+							27
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1324002570663329797/pu/img/3VuCAbBlEZnHXh-k.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1324002570663329797/pu/img/3VuCAbBlEZnHXh-k.jpg",
+						"url": "https://t.co/k6VMEYJaTw",
+						"display_url": "pic.twitter.com/k6VMEYJaTw",
+						"expanded_url": "https://twitter.com/bdsams/status/1324002646240485378/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 1324002646240485400,
+						"source_status_id_str": "1324002646240485378",
+						"source_user_id": 102800191,
+						"source_user_id_str": "102800191"
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1324002570663329800,
+						"id_str": "1324002570663329797",
+						"indices": [
+							4,
+							27
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1324002570663329797/pu/img/3VuCAbBlEZnHXh-k.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1324002570663329797/pu/img/3VuCAbBlEZnHXh-k.jpg",
+						"url": "https://t.co/k6VMEYJaTw",
+						"display_url": "pic.twitter.com/k6VMEYJaTw",
+						"expanded_url": "https://twitter.com/bdsams/status/1324002646240485378/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"source_status_id": 1324002646240485400,
+						"source_status_id_str": "1324002646240485378",
+						"source_user_id": 102800191,
+						"source_user_id_str": "102800191",
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 22000,
+							"variants": [
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1324002570663329797/pu/vid/480x270/5Kpt8W5M9Wj9s5OB.mp4?tag=10"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1324002570663329797/pu/vid/1280x720/NNVg7546l1eVh0hC.mp4?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1324002570663329797/pu/vid/640x360/6kRhZcAFIqlRjMyu.mp4?tag=10"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1324002570663329797/pu/pl/GzSrAc7ib_ARcXe4.m3u8?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false,
+							"source_user": {
+								"id": 102800191,
+								"id_str": "102800191",
+								"name": "Brad Sams",
+								"screen_name": "bdsams",
+								"location": "Cincinnati, Ohio",
+								"description": "Executive Editor of @bwwmediagroup | Wrote a Book - https://t.co/DsI2YitZJ3 | YouTube - https://t.co/IuGSp2JtQ2 | DM for Signal",
+								"url": null,
+								"entities": {
+									"description": {
+										"urls": [
+											{
+												"url": "https://t.co/DsI2YitZJ3",
+												"expanded_url": "http://bdsams.com/Book",
+												"display_url": "bdsams.com/Book",
+												"indices": [
+													52,
+													75
+												]
+											},
+											{
+												"url": "https://t.co/IuGSp2JtQ2",
+												"expanded_url": "http://bdsams.com/YouTube",
+												"display_url": "bdsams.com/YouTube",
+												"indices": [
+													88,
+													111
+												]
+											}
+										]
+									}
+								},
+								"protected": false,
+								"followers_count": 34564,
+								"friends_count": 570,
+								"listed_count": 685,
+								"created_at": "Thu Jan 07 21:47:50 +0000 2010",
+								"favourites_count": 42863,
+								"utc_offset": null,
+								"time_zone": null,
+								"geo_enabled": true,
+								"verified": true,
+								"statuses_count": 92003,
+								"lang": null,
+								"contributors_enabled": false,
+								"is_translator": false,
+								"is_translation_enabled": false,
+								"profile_background_color": "F5F5F5",
+								"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+								"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+								"profile_background_tile": false,
+								"profile_image_url": "http://pbs.twimg.com/profile_images/1197856518684790785/s6154zUQ_normal.jpg",
+								"profile_image_url_https": "https://pbs.twimg.com/profile_images/1197856518684790785/s6154zUQ_normal.jpg",
+								"profile_banner_url": "https://pbs.twimg.com/profile_banners/102800191/1406509372",
+								"profile_link_color": "4F0DA6",
+								"profile_sidebar_border_color": "FFFFFF",
+								"profile_sidebar_fill_color": "EFEFEF",
+								"profile_text_color": "333333",
+								"profile_use_background_image": false,
+								"has_extended_profile": true,
+								"default_profile": false,
+								"default_profile_image": false,
+								"following": false,
+								"follow_request_sent": false,
+								"notifications": false,
+								"translator_type": "none"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 948104532,
+				"id_str": "948104532",
+				"name": "Earthling",
+				"screen_name": "ziyatong",
+				"location": "Laniakea Supercluster",
+				"description": "Author of The Reality Bubble. TV Presenter. Boards: @WWF Intl, @thenarwhalca, @PENCanada, @WeAnimals Fmr Vice Chair @WWFCanada",
+				"url": "https://t.co/NXdhsSCI5x",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/NXdhsSCI5x",
+								"expanded_url": "https://www.indiebound.org/book/9780735235564?aff=penguinrandom",
+								"display_url": "indiebound.org/book/978073523…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 40538,
+				"friends_count": 2272,
+				"listed_count": 665,
+				"created_at": "Wed Nov 14 17:03:42 +0000 2012",
+				"favourites_count": 46059,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 58982,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "09BEDE",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1164720962677297152/lLWyZByw_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1164720962677297152/lLWyZByw_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/948104532/1555102814",
+				"profile_link_color": "001F3F",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4280,
+			"favorite_count": 13658,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "und"
+		},
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is the only time a cat has ever crossed over into “acceptable” for me. This is the good cat. https://t.co/N9kwMG8IDZ"
+	},
+	{
+		"created_at": "Wed Nov 04 16:51:05 +0000 2020",
+		"id": 1324031422517121000,
+		"id_str": "1324031422517121026",
+		"full_text": "@molly_knight Honestly no, not without the senate. Well, maybe a little better.",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			79
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "molly_knight",
+					"name": "Molly Knight",
+					"id": 45882011,
+					"id_str": "45882011",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1324025256999153700,
+		"in_reply_to_status_id_str": "1324025256999153670",
+		"in_reply_to_user_id": 45882011,
+		"in_reply_to_user_id_str": "45882011",
+		"in_reply_to_screen_name": "molly_knight",
+		"user": {
+			"id": 90161583,
+			"id_str": "90161583",
+			"name": "Mike Carlucci",
+			"screen_name": "mikecarlucci",
+			"location": "Boston, MA",
+			"description": "Writing about the Red Sox @OverTheMonster Data Analyst @Northeastern. Co-host @ozminute mike@mikecarlucci.org\n📷mikegcarlucci",
+			"url": "https://t.co/AvA6PiVYL1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/AvA6PiVYL1",
+							"expanded_url": "https://www.sbnation.com/users/mikecarlucci/",
+							"display_url": "sbnation.com/users/mikecarl…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4452,
+			"friends_count": 755,
+			"listed_count": 46,
+			"created_at": "Sun Nov 15 13:32:20 +0000 2009",
+			"favourites_count": 82802,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17556,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/824221611960176641/-9PsagQw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/90161583/1590433607",
+			"profile_link_color": "0A0AFF",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EDE8ED",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@molly_knight Honestly no, not without the senate. Well, maybe a little better."
+	},
+	{
 		"created_at": "Tue Nov 03 23:08:25 +0000 2020",
 		"id": 1323763996307148800,
 		"id_str": "1323763996307148805",
