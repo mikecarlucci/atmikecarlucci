bd520fc08fa3ae476357bc3c384bfa82a6543759
Mike Carlucci <mike@mikecarlucci.org>
2021-01-02 22:10:20 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22494,
-	"created_at": "Sat Jan 02 03:10:19 +0000 2021",
+	"tweets": 22495,
+	"created_at": "Sun Jan 03 03:10:19 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,129 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sun Jan 03 02:30:57 +0000 2021",
+		"id": 1103366295830085600,
+		"id_str": "1345558236779454464",
+		"full_text": "The Matrix: This guy is a slave to the system\n\nThe Matrix 2019: Wow this guy has his own cube at work",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			101
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2436389418,
+			"id_str": "2436389418",
+			"name": "SwiftOnSecurity",
+			"screen_name": "SwiftOnSecurity",
+			"location": "Cyber, USA",
+			"description": "Systems security, author https://t.co/LUiVUeE6gN, https://t.co/rzDVLlLn6y, SciFi @ https://t.co/drx75IY7eS, sysadmin, & SecEng. Microsoft MVP, Client Security 2021. they/them/tay",
+			"url": "https://t.co/8OV6y5BH0K",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/8OV6y5BH0K",
+							"expanded_url": "https://decentsecurity.com",
+							"display_url": "decentsecurity.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/LUiVUeE6gN",
+							"expanded_url": "http://DecentSecurity.com",
+							"display_url": "DecentSecurity.com",
+							"indices": [
+								25,
+								48
+							]
+						},
+						{
+							"url": "https://t.co/rzDVLlLn6y",
+							"expanded_url": "http://GotPhish.com",
+							"display_url": "GotPhish.com",
+							"indices": [
+								50,
+								73
+							]
+						},
+						{
+							"url": "https://t.co/drx75IY7eS",
+							"expanded_url": "http://UniversalShards.com",
+							"display_url": "UniversalShards.com",
+							"indices": [
+								83,
+								106
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 320125,
+			"friends_count": 8807,
+			"listed_count": 6572,
+			"created_at": "Thu Apr 10 02:54:26 +0000 2014",
+			"favourites_count": 110280,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 129484,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1337666327625015296/YQkdjc9n_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1337666327625015296/YQkdjc9n_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2436389418/1598152150",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 905,
+		"favorite_count": 3740,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "The Matrix: This guy is a slave to the system\n\nThe Matrix 2019: Wow this guy has his own cube at work"
+	},
+	{
 		"created_at": "Sat Jan 02 00:58:26 +0000 2021",
 		"id": 1345172564910497800,
 		"id_str": "1345172564910497792",
