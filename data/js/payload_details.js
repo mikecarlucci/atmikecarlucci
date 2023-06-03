908d08a3af2b720613154c8c208d35b4f1c53c13
Mike Carlucci <mike@mikecarlucci.org>
2023-06-02 23:10:20 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 25937,
-	"created_at": "Thu Jun 01 03:10:19 +0000 2023",
+	"tweets": 25938,
+	"created_at": "Sat Jun 03 03:10:19 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_06.js
@@ -0,0 +1,290 @@
+Grailbird.data.tweets_2023_06 = 
+[
+	{
+		"created_at": "Fri Jun 02 23:43:17 +0000 2023",
+		"id": 1664779751876173800,
+		"id_str": "1664779751876173825",
+		"full_text": "https://t.co/v1pr4cv1o8 https://t.co/rOwsvOfvxP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			23
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/v1pr4cv1o8",
+					"expanded_url": "https://twitter.com/danielle_lerner/status/1664739461639307264",
+					"display_url": "twitter.com/danielle_lerne…",
+					"indices": [
+						0,
+						23
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1664779749397262300,
+					"id_str": "1664779749397262337",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/Fxp8XJgWcAEnhZY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fxp8XJgWcAEnhZY.jpg",
+					"url": "https://t.co/rOwsvOfvxP",
+					"display_url": "pic.twitter.com/rOwsvOfvxP",
+					"expanded_url": "https://twitter.com/mikecarlucci/status/1664779751876173825/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1664779749397262300,
+					"id_str": "1664779749397262337",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/Fxp8XJgWcAEnhZY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fxp8XJgWcAEnhZY.jpg",
+					"url": "https://t.co/rOwsvOfvxP",
+					"display_url": "pic.twitter.com/rOwsvOfvxP",
+					"expanded_url": "https://twitter.com/mikecarlucci/status/1664779751876173825/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 90161583,
+			"id_str": "90161583",
+			"name": "mikecarlucci@journa.host",
+			"screen_name": "mikecarlucci",
+			"location": "Boston, MA",
+			"description": "Writer at SBNation's @OverTheMonster | Data Analyst @northeastern | @l_kyllonen is my better half 📧: mike@mikecarlucci.org | @mikecarlucci@journa.host",
+			"url": "https://t.co/u2JL5kNBEP",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/u2JL5kNBEP",
+							"expanded_url": "https://www.overthemonster.com",
+							"display_url": "overthemonster.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4309,
+			"friends_count": 653,
+			"listed_count": 48,
+			"created_at": "Sun Nov 15 13:32:20 +0000 2009",
+			"favourites_count": 114086,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 21281,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1594471376072724482/Pm9H00sF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1594471376072724482/Pm9H00sF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/90161583/1637537262",
+			"profile_link_color": "0A0AFF",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EDE8ED",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1664739461639307300,
+		"quoted_status_id_str": "1664739461639307264",
+		"quoted_status_permalink": {
+			"url": "https://t.co/v1pr4cv1o8",
+			"expanded": "https://twitter.com/danielle_lerner/status/1664739461639307264",
+			"display": "twitter.com/danielle_lerne…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jun 02 21:03:11 +0000 2023",
+			"id": 1664739461639307300,
+			"id_str": "1664739461639307264",
+			"full_text": "Dusty Baker on Shohei Ohtani: “He could probably outrun a bear.”",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				64
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 855968208,
+				"id_str": "855968208",
+				"name": "Danielle Lerner",
+				"screen_name": "danielle_lerner",
+				"location": "SJ-CoMo-LOU-MEM- HOU",
+				"description": "Sports enterprise reporter @HoustonChron. Past: @dailymemphian, @TheAthleticCBB, @courierjournal. #MizzouMade 🏳️‍🌈 Email: danielle.lerner@houstonchronicle.com",
+				"url": "https://t.co/V7I7lRHBfR",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/V7I7lRHBfR",
+								"expanded_url": "https://www.houstonchronicle.com/author/danielle-lerner/",
+								"display_url": "houstonchronicle.com/author/daniell…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 11335,
+				"friends_count": 3561,
+				"listed_count": 253,
+				"created_at": "Mon Oct 01 07:16:56 +0000 2012",
+				"favourites_count": 21584,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 37357,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1181198068303183872/BSkSD6Ih_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1181198068303183872/BSkSD6Ih_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/855968208/1658943463",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 40,
+			"favorite_count": 288,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "zxx",
+		"text": "https://t.co/v1pr4cv1o8 https://t.co/rOwsvOfvxP"
+	}
+]
